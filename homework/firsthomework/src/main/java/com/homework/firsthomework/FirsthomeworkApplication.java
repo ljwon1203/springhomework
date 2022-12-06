ed659480e39bc46ebcc2b43142bd2c69e6def7e6
Jaewon Lee <ljwon1203@gmail.com>
2022-12-06 17:16:35 +0900

    homework first commit

---
+++ homework/firsthomework/src/main/java/com/homework/firsthomework/FirsthomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.homework.firsthomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirsthomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirsthomeworkApplication.class, args);
+	}
+
+}
